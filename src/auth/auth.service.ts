84927161e5a57a87ec68a8115df26390aeb368ac
IRSAGE <sibo.egide98@gmail.com>
2023-08-13 22:28:42 +0200

    SignUp Logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { msg: 'I am signed up' };
+
+  async signUp(dto: AuthDto) {
+    //generate the password
+    const hash = await argon.hash(dto.password);
+
+    try {
+      //save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        //setting the objets to be returned
+        //   select: {
+        //     id: true,
+        //     email: true,
+        //     createdAt: true,
+        //   },
+      });
+      //remove hashed password in the returned data
+      delete user.hash;
+      //return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials Taken,Please try another email',
+          );
+        }
+      }
+    }
   }
 
-  signUp() {
-    return 'I am signed in';
+  signin() {
+    return { msg: 'I am signed up' };
   }
 }
