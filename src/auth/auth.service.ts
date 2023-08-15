d4a78b416f597462b9962ac1c01da9bab3f3d198
IRSAGE <sibo.egide98@gmail.com>
2023-08-15 09:48:33 +0200

    SignIn with Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signUp(dto: AuthDto) {
     //generate the password
@@ -27,9 +33,12 @@ export class AuthService {
         //   },
       });
       //remove hashed password in the returned data
-      delete user.hash;
+      //delete user.hash;
       //return the saved user
-      return user;
+      // return user
+
+      // return token
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -39,6 +48,26 @@ export class AuthService {
         }
       }
     }
+  }
+
+  //Generating Tokem
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string; message: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return {
+      message: 'Logged in successfully',
+      access_token: token,
+    };
   }
 
   async signin(dto: AuthDto) {
@@ -61,8 +90,10 @@ export class AuthService {
       );
     }
 
+    // return token
+    return this.signToken(user.id, user.email);
     //send back the user
-    delete user.hash;
-    return user;
+    // delete user.hash;
+    // return user;
   }
 }
